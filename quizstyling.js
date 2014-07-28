18584fc52d31bd7f185b803db1a6587811912545
MattHarbert <MatthewHarbert@gmail.com>
2014-07-28 16:51:48 -0700

    Fixes, Mobile, & Container
    Made some fixes with IE compatibility of the image selector, updated the
    mobile styling of the quiz pages, and created the HTML/CSS for the
    section that will hold all of the quiz content.

--- quizstyling.js
+++ quizstyling.js
@@ -1,20 +1,25 @@
 // JavaScript Document
 
-$("#save_quiz_answer li.answer label input").not(':checkbox').click(function(){										
-												
-												if($(this).parents(".imagesonechoice").length>0){
-													$("#save_quiz_answer li.answer ul li").not($(this).parents("li.answer")).removeClass("answer active");
-													$(this).parents("li.answer ul li").addClass("answer active");
+$("#save_quiz_answer li.answer label").click(function(){										
+												var thisinput=$(this).children("input");
+												if(thisinput.not(':checkbox').length>0){
+												if(thisinput.parents(".imagesonechoice").length>0){
+													$("#save_quiz_answer li.answer ul li").not(thisinput.parents("li.answer")).removeClass("answer active");
+													thisinput.parents("li.answer ul li").addClass("answer active");
 												}else{
-												$("#save_quiz_answer li.answer").not($(this).parents("li.answer")).removeClass("active");
-												$(this).parents("li.answer").addClass("active");
-												}
+												$("#save_quiz_answer li.answer").not(thisinput.parents("li.answer")).removeClass("active");
+												thisinput.parents("li.answer").addClass("active");
+												}}
 												});
 
 
-$("#save_quiz_answer li.answer ul li label input[type=checkbox]").click(function(){
-												$(this).parent("label").parent("li").toggleClass("active answer");
+$("#save_quiz_answer li.answer ul li label").has("input[type=checkbox]").click(function(evt){
+												evt.stopPropagation();
+												evt.preventDefault();
+												$(this).parent("li").toggleClass("active answer");								
 												});
+
+
 
 
 
